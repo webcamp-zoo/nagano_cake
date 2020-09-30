3b56013490614be58fbffc6c37ea8f65954decf9
vagrant <vagrant@localhost.localdomain>
2020-09-30 12:28:31 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_09_30_082626) do
-
-  create_table "cart_products", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "customer_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-ActiveRecord::Schema.define(version: 2020_09_30_082154) do
+ActiveRecord::Schema.define(version: 2020_09_30_100505) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -44,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_09_30_082154) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "customer_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -51,14 +47,6 @@ ActiveRecord::Schema.define(version: 2020_09_30_082154) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "customers"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "address"
-    t.string "phone_number"
-    t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_deleted", default: true, null: false
@@ -66,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_09_30_082154) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.integer "making_status"
+    t.integer "taxed_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id"
@@ -83,7 +80,9 @@ ActiveRecord::Schema.define(version: 2020_09_30_082154) do
   create_table "product_genres", force: :cascade do |t|
     t.integer "name", default: 0
     t.boolean "is_active", default: true
-
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.boolean "products", default: true
@@ -93,15 +92,6 @@ ActiveRecord::Schema.define(version: 2020_09_30_082154) do
     t.string "image_id"
     t.text "introduction"
     t.boolean "is_active", default: true
-
-  create_table "order_products", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "order_id"
-    t.integer "quantity"
-    t.integer "making_status"
-    t.integer "taxed_price"
-
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
