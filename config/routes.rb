1fb4be14303e857dcd7441aeef9402084b028cae
vagrant <vagrant@localhost.localdomain>
2020-10-01 07:09:42 +0000

    customer.rb product.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do    resources :customers, only:[:index]     
+  end
 end
