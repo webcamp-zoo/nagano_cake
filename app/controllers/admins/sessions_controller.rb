7892044c447fed4faa5f2d69819bf2ad2c7c595b
vagrant <vagrant@localhost.localdomain>
2020-10-01 07:16:46 +0000

    add

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -24,4 +24,15 @@ class Admins::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
+  #ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    products_top_path
+  end
+
+  #ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    リダイレクト先のパス
+  end
 end
