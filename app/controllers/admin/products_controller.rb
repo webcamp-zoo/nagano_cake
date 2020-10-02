3c26e14fe8cd2b5fa7f34c2cb7af3f859348a0eb
vagrant <vagrant@localhost.localdomain>
2020-10-02 02:39:07 +0000

    yu2

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,23 @@
 class Admin::ProductsController < ApplicationController
 
-	def top
-	end
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product) notice: "You have created new genre successfully."
+    else
+      redirect_to request.referer
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :image_id, :introduction,
+       :genre_id, :price, :is_active)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
   	resources :product_genres, only: [:index,:create,:edit,:update]
+  	resources :products, only: [:new,:create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
