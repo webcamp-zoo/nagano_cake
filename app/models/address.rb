cd46d77a5653d8fa9aaaf866f9643985d07df4a3
vagrant <vagrant@localhost.localdomain>
2020-10-11 05:47:36 +0000

    add

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,9 @@
 class Address < ApplicationRecord
-	
+
 	belongs_to :customer
+
+	validates :customer_id, :name, :address, :postal_code, presence: true
+	validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
 
 	def order_address
 		self.postal_code + self.address + self.name
